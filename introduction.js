d9dd9361bf11038bc9625d3353998a16a1728610
Satinder0365 <123872537+Satinder0365@users.noreply.github.com>
2024-01-22 21:36:49 +0530

    Starting project

---
+++ introduction.js
@@ -0,0 +1,3 @@
+//In javaScript console.log is used to print output
+ 
+console.log('This is my 30 Days JavaScript Challenge')
---
+++ main.js
@@ -0,0 +1,3 @@
+console.log(firstName, lastName, state, city, age, marriedstatus);
+console.log(citycode, houseno, PI);
+console.log( newname, Work, rollno);
